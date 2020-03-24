b62a7d9953f1b213923c5b7b34947ec2d26ee84d
DawnGrade <37818718+ThiagoAlencar12@users.noreply.github.com>
2020-03-24 10:35:46 -0300

    Resolução

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,23 +6,20 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       rua: Yup.string().required(),
-      num_casa: Yup.number().required(),
+      num_casa: Yup.string().required(),
       telefone: Yup.string().required(),
       login: Yup.string().required(),
       password: Yup.string().required(),
     });
 
-    //Condição de validação dos campos e dados de entrada
     if(!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Falha na validação, dado inválido' });
+      return res.status(400).json({ message: "Dados inválidos" })
     }
-    
-    // Validando o user
-    const usuarioExiste = await User.findOne({ where: { email: req.body.email } });
 
-    // Condição para ver se o user já existe
-    if(usuarioExiste) {
-      return res.status(400).json({ error: 'Usuário já existe!' });
+    const userExiste = await User.findOne({ where: { login: req.body.login } });
+
+    if(userExiste) {
+      return res.status(400).json({ error : "Usuario existe!" });
     }
 
     const {id, nome, login , email} = await User.create(req.body);
